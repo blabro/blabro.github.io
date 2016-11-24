df72ca6e6f3525ad186c75798bad0bbcf11737db
blabro <blabro@o2.pl>
2016-11-24 11:26:21 +0100

    Add files via upload

--- js/myangular.js
+++ js/myangular.js
@@ -9,10 +9,10 @@ function scrollContainer($window) {
         link: function(scope, element, attrs) {
             angular.element($window).bind("scroll", function() {
                 $('.fadingIn').each( function(i){
-                    var bottom_of_object = $(this).offset().top + $(this).outerHeight()*3/4;
+                    var bottom_of_object = $(this).offset().top + $(this).outerHeight()*1/4;
                     var bottom_of_window = $(window).scrollTop() + $(window).height();
                     if( bottom_of_window > bottom_of_object ){
-                        $(this).animate({'opacity':'1'},500);
+                        $(this).animate({'opacity':'1'},1500);
                     }
                });
            });
